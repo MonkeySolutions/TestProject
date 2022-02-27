7b97c9477f4dfd5da69ffee055db013679e627ae
filoingko <llchathu37@gmail.com>
2022-02-28 01:31:36 +0530

    Add addSomething() method

--- TestCode.java
+++ TestCode.java
@@ -2,5 +2,9 @@ class TestCode{
  public static void main(String args[]){
   System.out.println("This is a test code"); 
    
- } 
+ }
+
+ public void addSomething(){
+     System.out.println("Something");
+ }
 }
